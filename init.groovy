bf29a8eb4363d110e87916f363eaba0bb9782352
nduperre <nicolas.duperre@gologic.ca>
2018-11-29 13:30:04 -0500

    Ajouter la générateur de tâches

--- init.groovy
+++ init.groovy
@@ -7,11 +7,11 @@ import hudson.PluginManager
 import hudson.security.*
 
 String PIPELINE_LIB_ID = "PIPELIB"
-String GIT_URL_PIPELINE_LIB = "https://github.com/gologic-nico/jenkins-pipeline-library.git"
-String GIT_PASSWORD = ""
+String PIPELINE_DEFAULT_BRANCH = "master"
+String PIPELINE_LIB_GIT_URL = "https://github.com/gologic-nico/jenkins-pipeline-library.git"
+String JENKINS_ADMIN_USERNAME = "admin"
+String JENKINS_ADMIN_PASSWORD = "admin"
 def pluginsList = ["ace-editor","analysis-core","ant","antisamy-markup-formatter","apache-httpcomponents-client-4-api","authentication-tokens","blueocean","blueocean-autofavorite","blueocean-bitbucket-pipeline","blueocean-commons","blueocean-config","blueocean-core-js","blueocean-dashboard","blueocean-display-url","blueocean-events","blueocean-github-pipeline","blueocean-git-pipeline","blueocean-i18n","blueocean-jira","blueocean-jwt","blueocean-personalization","blueocean-pipeline-api-impl","blueocean-pipeline-editor","blueocean-pipeline-scm-api","blueocean-rest","blueocean-rest-impl","blueocean-web","bouncycastle-api","branch-api","build-timeout","cloudbees-bitbucket-branch-source","cloudbees-folder","command-launcher","config-file-provider","credentials","credentials-binding","display-url-api","docker-commons","docker-java-api","docker-plugin","docker-workflow","durable-task","email-ext","favorite","git","git-client","github","github-api","github-branch-source","git-server","gradle","handlebars","handy-uri-templates-2-api","htmlpublisher","jackson2-api","javadoc","jdk-tool","jenkins-design-language","jira","jquery-detached","jsch","junit","ldap","lockable-resources","mailer","mapdb-api","matrix-auth","matrix-project","maven-plugin","mercurial","momentjs","msbuild","pam-auth","pipeline-build-step","pipeline-github-lib","pipeline-graph-analysis","pipeline-input-step","pipeline-milestone-step","pipeline-model-api","pipeline-model-declarative-agent","pipeline-model-definition","pipeline-model-extensions","pipeline-rest-api","pipeline-stage-step","pipeline-stage-tags-metadata","pipeline-stage-view","plain-credentials","pubsub-light","resource-disposer","scm-api","script-security","sse-gateway","ssh-credentials","ssh-slaves","structs","subversion","timestamper","token-macro","trilead-api","variant","workflow-aggregator","workflow-api","workflow-basic-steps","workflow-cps","workflow-cps-global-lib","workflow-durable-task-step","workflow-job","workflow-multibranch","workflow-scm-step","workflow-step-api","workflow-support","ws-cleanup", "robot"]
-String ADMIN_USERNAME = "admin"
-String ADMIN_USERNAMEPASSWORD = "admin"
 
 println "---------------------------------------------------------------"
 println "-"
@@ -21,17 +21,17 @@ println "---------------------------------------------------------------"
 
 def JENKINS = Jenkins.get()
 
-println "Set INITIAL_PLUGINS_INSTALLING"
+println "--------------- Set INITIAL_PLUGINS_INSTALLING"
 try {
     JENKINS.setInstallState(InstallState.INITIAL_PLUGINS_INSTALLING)
     println "setInstallState INITIAL_PLUGINS_INSTALLING done!"
 }
 catch (Exception e)  {
-    println "=========Error INITIAL_PLUGINS_INSTALLING ========= : \n" + e.printStackTrace()
+    println "---------------Error INITIAL_PLUGINS_INSTALLING --------------- : \n" + e.printStackTrace()
 }
 JENKINS.save()
 
-println "Start install all plugins!"
+println "--------------- Start install all plugins!"
 UpdateSite updateSite = JENKINS.get().getUpdateCenter().getById('default')
 PluginManager pluginManager = JENKINS.getPluginManager()
 
@@ -39,44 +39,44 @@ for (pluginId in pluginsList) {
 
     if (pluginManager.getPlugin(pluginId) == null ) {
 
-        println "Install start install plugins : ${pluginId}"
+        println "--------------- Install start install plugins : ${pluginId}"
         UpdateSite.Plugin plugin = updateSite.getPlugin(pluginId)
         Throwable error = plugin.deploy(true).get().getError()
 
         if(error != null) {
-            println "ERROR installing ${pluginId} : ${error}"
+            println "--------------- ERROR installing ${pluginId} : ${error}"
         } else{
-            println "Successfully installed plugin ${pluginId}"
+            println "--------------- Successfully installed plugin ${pluginId}"
         }
     }
     else{
-            println "Plugin ${pluginId} already installed"
-        }
+        println "--------------- Plugin ${pluginId} already installed"
+    }
 }
 JENKINS.save()
 
-println "Set CREATE_ADMIN_USER"
+println "--------------- Set CREATE_ADMIN_USER"
 try {
     JENKINS.setInstallState(InstallState.CREATE_ADMIN_USER)
-    println "setInstallState CREATE_ADMIN_USER done!"
+    println "--------------- setInstallState CREATE_ADMIN_USER done!"
 }
 catch (Exception e)  {
-    println "=========Error CREATE_ADMIN_USER ========= : \n" + e.printStackTrace()
+    println "---------------Error CREATE_ADMIN_USER --------------- : \n" + e.printStackTrace()
 }
 JENKINS.save()
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount(ADMIN_USERNAME, ADMIN_USERNAMEPASSWORD)
+hudsonRealm.createAccount(JENKINS_ADMIN_USERNAME, JENKINS_ADMIN_PASSWORD)
 JENKINS.setSecurityRealm(hudsonRealm)
 JENKINS.save()
 
-println "Set INITIAL_SETUP_COMPLETED"
+println "--------------- Set INITIAL_SETUP_COMPLETED"
 try {
     JENKINS.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
-    println "setInstallState INITIAL_SETUP_COMPLETED done!"
+    println "--------------- setInstallState INITIAL_SETUP_COMPLETED done!"
 }
 catch (Exception e)  {
-    println "=========Error INITIAL_SETUP_COMPLETED ========= : \n" + e.printStackTrace()
+    println "--------------- Error INITIAL_SETUP_COMPLETED --------------- : \n" + e.printStackTrace()
 }
 
 JENKINS.setNumExecutors(10)
@@ -84,6 +84,49 @@ JENKINS.setLabelString("master jenkins-pipeline-library")
 
 //Permet de visualiser les rapports HTML robot framework
 System.setProperty("hudson.model.DirectoryBrowserSupport.CSP", "")
+
+JENKINS.save()
+
+String dslGeneratorJobName = "DSL-Generator"
+TopLevelItem dslGeneratorJob = JENKINS.getItem(dslGeneratorJobName)
+
+if(dslGeneratorJob == null){
+	dslGeneratorJob = JENKINS.createProject(hudson.model.FreeStyleProject.class, dslGeneratorJobName)
+	dslGeneratorJob.setDescription("Générateur DSL de tâches Jenkins")
+	dslGeneratorJob.setAssignedLabel(new LabelAtom("master"))
+
+	LogRotator logRotator = new LogRotator(-1, 50, -1, -1)
+	dslGeneratorJob.setLogRotator(logRotator)
+	
+	SCMTrigger scmTrigger = new SCMTrigger("* * * * *")
+	scmTrigger.start(dslGeneratorJob, true)
+	dslGeneratorJob.addTrigger(scmTrigger)
+	
+	List<BranchSpec> branches = [
+		new BranchSpec(PIPELINE_DEFAULT_BRANCH)
+	]
+	dslGeneratorJob.setScm(new GitSCM(GitSCM.createRepoList(PIPELINE_LIB_GIT_URL, ""), branches, false, null, null, null, null))
+
+	ExecuteDslScripts executeDslScripts = new ExecuteDslScripts()
+	executeDslScripts.setTargets("src/mtl/devops/dslGeneratorJobs.groovy")
+	executeDslScripts.setRemovedJobAction(RemovedJobAction.DELETE)
+	executeDslScripts.setRemovedViewAction(RemovedViewAction.DELETE)
+	executeDslScripts.setRemovedConfigFilesAction(RemovedConfigFilesAction.DELETE)
+	executeDslScripts.setAdditionalClasspath("src")
+	dslGeneratorJob.getBuildersList().add(executeDslScripts)
+
+	dslGeneratorJob.save()
+	dslGeneratorJob.scheduleBuild(0)
+}
+
+def globalConfigFileStore = org.jenkinsci.plugins.configfiles.GlobalConfigFiles.get();
+
+def stringWriter = new StringWriter()
+def settingsXmlFile = new XmlSlurper().parse(new File("~/jenkins-pipeline-library/ressources/settings.xml"))
+new XmlNodePrinter(new PrintWriter(stringWriter)).print(settingsXmlFile)
+
+def mavenSettingsConfig = new org.jenkinsci.plugins.configfiles.maven.MavenSettingsConfig("nexus-settings", "nexus-settings", "", stringWriter.toString(), null, null);
+globalConfigFileStore.save(mavenSettingsConfig)
 
 JENKINS.save()
 
---
+++ src/mtl/devops/dslGeneratorJobs.groovy
@@ -0,0 +1,17 @@
+#!groovy
+
+def projectsJson = this.readFileFromWorkspace("ressources/projects.json")
+def root = new groovy.json.JsonSlurperClassic().parseText(projectsJson)
+
+//Create MultiBranch pipeline
+for (projet in root.projets) {
+
+	def multibranchPipelineJob = multibranchPipelineJob(projet.name) {
+		branchSources {
+			git { remote(projet.gitUrl) }
+		}
+		orphanedItemStrategy {
+			discardOldItems { numToKeep(20) }
+		}
+	}
+}
