bd53df29fd4a4167c781632661f71e3b201307cb
nduperre <nicolas.duperre@gologic.ca>
2018-11-29 13:35:41 -0500

    Ajouter le path absolue

--- init.groovy
+++ init.groovy
@@ -11,6 +11,8 @@ String PIPELINE_DEFAULT_BRANCH = "master"
 String PIPELINE_LIB_GIT_URL = "https://github.com/gologic-nico/jenkins-pipeline-library.git"
 String JENKINS_ADMIN_USERNAME = "admin"
 String JENKINS_ADMIN_PASSWORD = "admin"
+String MAVEN_SETTINGS_ABSOLUTE_PATH = "/var/lib/jenkins/jenkins-pipeline-library/ressources/settings.xml"
+
 def pluginsList = ["ace-editor","analysis-core","ant","antisamy-markup-formatter","apache-httpcomponents-client-4-api","authentication-tokens","blueocean","blueocean-autofavorite","blueocean-bitbucket-pipeline","blueocean-commons","blueocean-config","blueocean-core-js","blueocean-dashboard","blueocean-display-url","blueocean-events","blueocean-github-pipeline","blueocean-git-pipeline","blueocean-i18n","blueocean-jira","blueocean-jwt","blueocean-personalization","blueocean-pipeline-api-impl","blueocean-pipeline-editor","blueocean-pipeline-scm-api","blueocean-rest","blueocean-rest-impl","blueocean-web","bouncycastle-api","branch-api","build-timeout","cloudbees-bitbucket-branch-source","cloudbees-folder","command-launcher","config-file-provider","credentials","credentials-binding","display-url-api","docker-commons","docker-java-api","docker-plugin","docker-workflow","durable-task","email-ext","favorite","git","git-client","github","github-api","github-branch-source","git-server","gradle","handlebars","handy-uri-templates-2-api","htmlpublisher","jackson2-api","javadoc","jdk-tool","jenkins-design-language","jira","jquery-detached","jsch","junit","ldap","lockable-resources","mailer","mapdb-api","matrix-auth","matrix-project","maven-plugin","mercurial","momentjs","msbuild","pam-auth","pipeline-build-step","pipeline-github-lib","pipeline-graph-analysis","pipeline-input-step","pipeline-milestone-step","pipeline-model-api","pipeline-model-declarative-agent","pipeline-model-definition","pipeline-model-extensions","pipeline-rest-api","pipeline-stage-step","pipeline-stage-tags-metadata","pipeline-stage-view","plain-credentials","pubsub-light","resource-disposer","scm-api","script-security","sse-gateway","ssh-credentials","ssh-slaves","structs","subversion","timestamper","token-macro","trilead-api","variant","workflow-aggregator","workflow-api","workflow-basic-steps","workflow-cps","workflow-cps-global-lib","workflow-durable-task-step","workflow-job","workflow-multibranch","workflow-scm-step","workflow-step-api","workflow-support","ws-cleanup", "robot"]
 
 println "---------------------------------------------------------------"
@@ -122,7 +124,7 @@ if(dslGeneratorJob == null){
 def globalConfigFileStore = org.jenkinsci.plugins.configfiles.GlobalConfigFiles.get();
 
 def stringWriter = new StringWriter()
-def settingsXmlFile = new XmlSlurper().parse(new File("~/jenkins-pipeline-library/ressources/settings.xml"))
+def settingsXmlFile = new XmlSlurper().parse(new File(MAVEN_SETTINGS_ABSOLUTE_PATH))
 new XmlNodePrinter(new PrintWriter(stringWriter)).print(settingsXmlFile)
 
 def mavenSettingsConfig = new org.jenkinsci.plugins.configfiles.maven.MavenSettingsConfig("nexus-settings", "nexus-settings", "", stringWriter.toString(), null, null);
