89922996a9fadc2ff7ce557c5f4b692310851612
nduperre <nicolas.duperre@gologic.ca>
2018-12-05 23:39:50 -0500

    Fixe accès variable pluginlist

--- init.groovy.d/init.groovy
+++ init.groovy.d/init.groovy
@@ -8,6 +8,7 @@ import hudson.security.*
 
 String JENKINS_ADMIN_USERNAME = "admin"
 String JENKINS_ADMIN_PASSWORD = "admin"
+def pluginsList = ["ace-editor","analysis-core","ant","antisamy-markup-formatter","apache-httpcomponents-client-4-api","authentication-tokens","blueocean","blueocean-autofavorite","blueocean-bitbucket-pipeline","blueocean-commons","blueocean-config","blueocean-core-js","blueocean-dashboard","blueocean-display-url","blueocean-events","blueocean-github-pipeline","blueocean-git-pipeline","blueocean-i18n","blueocean-jira","blueocean-jwt","blueocean-personalization","blueocean-pipeline-api-impl","blueocean-pipeline-editor","blueocean-pipeline-scm-api","blueocean-rest","blueocean-rest-impl","blueocean-web","bouncycastle-api","branch-api","build-timeout","cloudbees-bitbucket-branch-source","cloudbees-folder","command-launcher","config-file-provider","credentials","credentials-binding","display-url-api","docker-commons","docker-java-api","docker-plugin","docker-workflow","durable-task","email-ext","favorite","git","git-client","github","github-api","github-branch-source","git-server","gradle","handlebars","handy-uri-templates-2-api","htmlpublisher","jackson2-api","javadoc","jdk-tool","jenkins-design-language","jira","jquery-detached","jsch","junit","ldap","lockable-resources","mailer","mapdb-api","matrix-auth","matrix-project","maven-plugin","mercurial","momentjs","msbuild","pam-auth","pipeline-build-step","pipeline-github-lib","pipeline-graph-analysis","pipeline-input-step","pipeline-milestone-step","pipeline-model-api","pipeline-model-declarative-agent","pipeline-model-definition","pipeline-model-extensions","pipeline-rest-api","pipeline-stage-step","pipeline-stage-tags-metadata","pipeline-stage-view","plain-credentials","pubsub-light","resource-disposer","scm-api","script-security","sse-gateway","ssh-credentials","ssh-slaves","structs","subversion","timestamper","token-macro","trilead-api","variant","workflow-aggregator","workflow-api","workflow-basic-steps","workflow-cps","workflow-cps-global-lib","workflow-durable-task-step","workflow-job","workflow-multibranch","workflow-scm-step","workflow-step-api","workflow-support","ws-cleanup","robot","job-dsl"]
 
 println "---------------------------------------------------------------"
 println "-"
@@ -23,7 +24,6 @@ println "--------------- Set INITIAL_PLUGINS_INSTALLING"
 try {
 	InstallUtil.proceedToNextStateFrom(JENKINS.getInstallState())
 
-	//JENKINS.setInstallState(InstallState.INITIAL_PLUGINS_INSTALLING)
 	println "setInstallState INITIAL_PLUGINS_INSTALLING Done!"
 }
 catch (Exception e)  {
@@ -64,7 +64,6 @@ println "--------------- Set CREATE_ADMIN_USER"
 try {
 	InstallUtil.proceedToNextStateFrom(JENKINS.getInstallState())
 
-	//JENKINS.setInstallState(InstallState.CREATE_ADMIN_USER)
 	println "--------------- setInstallState CREATE_ADMIN_USER Done!"
 }
 catch (Exception e)  {
@@ -86,7 +85,6 @@ println "--------------- Set INITIAL_SETUP_COMPLETED"
 try {
 	InstallUtil.proceedToNextStateFrom(JENKINS.getInstallState())
 
-	//JENKINS.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
 	println "--------------- setInstallState INITIAL_SETUP_COMPLETED Done!"
 }
 catch (Exception e)  {
@@ -98,125 +96,9 @@ println "--------------- JENKINS STATE : " + JENKINS.getInstallState().name()
 JENKINS.setNumExecutors(10)
 JENKINS.setLabelString("master jenkins-pipeline-library")
 
-//Permet de visualiser les rapports HTML robot framework
+//
+//	Permet de visualiser les rapports HTML robot framework
+//
 System.setProperty("hudson.model.DirectoryBrowserSupport.CSP", "")
 
 JENKINS.save()
-
-def pluginsList = [
-	"ace-editor",
-	"analysis-core",
-	"ant",
-	"antisamy-markup-formatter",
-	"apache-httpcomponents-client-4-api",
-	"authentication-tokens",
-	"blueocean",
-	"blueocean-autofavorite",
-	"blueocean-bitbucket-pipeline",
-	"blueocean-commons",
-	"blueocean-config",
-	"blueocean-core-js",
-	"blueocean-dashboard",
-	"blueocean-display-url",
-	"blueocean-events",
-	"blueocean-github-pipeline",
-	"blueocean-git-pipeline",
-	"blueocean-i18n",
-	"blueocean-jira",
-	"blueocean-jwt",
-	"blueocean-personalization",
-	"blueocean-pipeline-api-impl",
-	"blueocean-pipeline-editor",
-	"blueocean-pipeline-scm-api",
-	"blueocean-rest",
-	"blueocean-rest-impl",
-	"blueocean-web",
-	"bouncycastle-api",
-	"branch-api",
-	"build-timeout",
-	"cloudbees-bitbucket-branch-source",
-	"cloudbees-folder",
-	"command-launcher",
-	"config-file-provider",
-	"credentials",
-	"credentials-binding",
-	"display-url-api",
-	"docker-commons",
-	"docker-java-api",
-	"docker-plugin",
-	"docker-workflow",
-	"durable-task",
-	"email-ext",
-	"favorite",
-	"git",
-	"git-client",
-	"github",
-	"github-api",
-	"github-branch-source",
-	"git-server",
-	"gradle",
-	"handlebars",
-	"handy-uri-templates-2-api",
-	"htmlpublisher",
-	"jackson2-api",
-	"javadoc",
-	"jdk-tool",
-	"jenkins-design-language",
-	"jira",
-	"jquery-detached",
-	"jsch",
-	"junit",
-	"ldap",
-	"lockable-resources",
-	"mailer",
-	"mapdb-api",
-	"matrix-auth",
-	"matrix-project",
-	"maven-plugin",
-	"mercurial",
-	"momentjs",
-	"msbuild",
-	"pam-auth",
-	"pipeline-build-step",
-	"pipeline-github-lib",
-	"pipeline-graph-analysis",
-	"pipeline-input-step",
-	"pipeline-milestone-step",
-	"pipeline-model-api",
-	"pipeline-model-declarative-agent",
-	"pipeline-model-definition",
-	"pipeline-model-extensions",
-	"pipeline-rest-api",
-	"pipeline-stage-step",
-	"pipeline-stage-tags-metadata",
-	"pipeline-stage-view",
-	"plain-credentials",
-	"pubsub-light",
-	"resource-disposer",
-	"scm-api",
-	"script-security",
-	"sse-gateway",
-	"ssh-credentials",
-	"ssh-slaves",
-	"structs",
-	"subversion",
-	"timestamper",
-	"token-macro",
-	"trilead-api",
-	"variant",
-	"workflow-aggregator",
-	"workflow-api",
-	"workflow-basic-steps",
-	"workflow-cps",
-	"workflow-cps-global-lib",
-	"workflow-durable-task-step",
-	"workflow-job",
-	"workflow-multibranch",
-	"workflow-scm-step",
-	"workflow-step-api",
-	"workflow-support",
-	"ws-cleanup",
-	"robot",
-	"job-dsl"
-]
-
